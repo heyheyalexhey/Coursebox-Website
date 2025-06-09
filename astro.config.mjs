c704fca087fb0984c48b74669487e3755d3fafdf
heyheyalexhey <heyheyalexhey@gmail.com>
2025-06-09 11:41:58 +0800

    Update deployment configuration with GitHub's recommended workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://heyheyalexhey.github.io',
-  base: '/Coursebox-Website',
-  output: 'static',
-  build: {
-    assets: 'astro'
-  }
+  base: '/Coursebox-Website'
 }); 
