843e56e4becaefd77d3472f0fae59f5307e2ba56
heyheyalexhey <heyheyalexhey@gmail.com>
2025-06-09 11:25:12 +0800

    Configure for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://heyheyalexhey.github.io',
+  base: '/Coursebox-Website',
   output: 'static',
   build: {
-    format: 'directory',
-    client: './dist'
+    assets: 'astro'
   }
 }); 
